def79e0e43518a19a5b1c84d51d1c8162b7bf579
Unknown <ks.kennysoh@gmail.com>
2019-08-05 16:14:02 +0800

    VideoPlayer Change Cut Scene

---
+++ Assets/video.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class video : MonoBehaviour
+{
+    public double time;
+    public double currentTime;
+    private GameObject videoplayer; // GameObject having the attached video
+    private UnityEngine.Video.VideoPlayer videofile; // VideoPLayer component 
+    // Use this for initialization
+    void Start()
+    {
+        videoplayer = GameObject.Find("videoplayer"); // assigning GameObject
+        videofile = videoplayer.GetComponent<UnityEngine.Video.VideoPlayer>();
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentTime = videofile.time;
+        if (currentTime >= time)
+        {
+            Debug.Log("//Change To Next Scene");
+        }else
+        {
+            Debug.Log("Video Current Time: " + currentTime);
+        }
+    }
+}
